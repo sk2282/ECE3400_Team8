5c9dc86682fd81145fbae672fe6d8e065bb64798
Serena <skrech97>
2017-09-25 11:20:37 -0400

    moved comments in code

--- code/milestone2/milestone2_treasure/milestone2_treasure.ino
+++ code/milestone2/milestone2_treasure/milestone2_treasure.ino
@@ -21,31 +21,24 @@ void setup() {
 }
 
 void loop() {
-  while(1) { // reduces jitter
-    cli();  // UDRE interrupt slows this way down on arduino1.0
-    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+  while(1) {     // reduces jitter
+    cli();        // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) {      // save 256 samples
       while(!(ADCSRA & 0x10)); // wait for adc to be ready
-      ADCSRA = 0xf5; // restart adc
-      byte m = ADCL; // fetch adc data
+      ADCSRA = 0xf5;        // restart adc
+      byte m = ADCL;        // fetch adc data
       byte j = ADCH;
       int k = (j << 8) | m; // form into an int
-      k -= 0x0200; // form into a signed int
-      k <<= 6; // form into a 16b signed int
-      fft_input[i] = k; // put real data into even bins
-      fft_input[i+1] = 0; // set odd bins to 0
+      k -= 0x0200;          // form into a signed int
+      k <<= 6;              // form into a 16b signed int
+      fft_input[i] = k;     // put real data into even bins
+      fft_input[i+1] = 0;   // set odd bins to 0
     }
-    fft_window(); // window the data for better frequency response
-    fft_reorder(); // reorder the data before doing the fft
-    fft_run(); // process the data in the fft
-    fft_mag_log(); // take the output of the fft
+    fft_window();           // window the data for better frequency response
+    fft_reorder();          // reorder the data before doing the fft
+    fft_run();              // process the data in the fft
+    fft_mag_log();          // take the output of the fft
     sei();
-    //Serial.println("start");
-    
-    //for (byte i = 0 ; i < FFT_N/2 ; i++) { 
-    //  Serial.println(fft_log_out[i]); // send out the data
-    //}
-    
-
     /*
     7kHz: [44,49]
     12kHz: [78,82]
@@ -67,8 +60,6 @@ void loop() {
     }
     else {
       Serial.println("No Signal");
-    }
-    
-    
+    }   
   }
 }
