b1921ea4b78eb93ba3c7dca4d130acae335fa40e
as2855 <31287158+as2855@users.noreply.github.com>
2017-11-20 19:57:20 -0500

    add radio to milestone 4 updated code
    added the config stuff and the setup stuff

--- code/milestone4/milestone4/milestone4.ino
+++ code/milestone4/milestone4/milestone4.ino
@@ -22,13 +22,6 @@ int wallSensorL = 2; //13; // left wall sensor
 int frontThresh = 500; // threshold value for front wall sensor 
 int sideThresh = 375; // threshold value for side wall sensor threshold
 
-/*
-int lRead;
-int rRead;
-int lwRead;
-int rwRead;
-*/
-
 int frontRead; // for wall sensors
 int leftRead;
 int rightRead;
@@ -91,13 +84,53 @@ boolean notDone() {
   return false;
 }
 
+/** RADIO SENDER STUFF */
+// Hardware configuration
+RF24 radio(9,10); // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
+
+// Topology
+const uint64_t pipes[2] = { 0x0000000016LL, 0x0000000017LL }; // radio pipe addresses for the 2 nodes to communicate
+
+// Role management
+// Set up role.  This sketch uses the same software for all the nodes
+// in this system.  Doing so greatly simplifies testing.
+
+typedef enum { role_ping_out = 1, role_pong_back } role_e; // the various roles supported by this sketch
+const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"}; // the debug-friendly names of those roles
+role_e role = role_ping_out; // the role of the current running sketch
+
+
 void setup() {
   Serial.begin(9600);
-  /*
-  pinMode(wallSensorL, INPUT);
-  pinMode(wallSensorR, INPUT);
-  pinMode(wallSensorF, INPUT);
-  */
+  /** RADIO SENDER SETUP: setup and configure rf radio */
+  radio.begin();
+
+  // optionally, increase the delay between retries & # of retries
+  radio.setRetries(15,15);
+  radio.setAutoAck(true);
+  radio.setChannel(0x50); // set the channel
+  radio.setPALevel(RF24_PA_MIN); // set the power
+  radio.setDataRate(RF24_250KBPS);
+
+  // Open pipes to other nodes for communication
+  // This simple sketch opens two pipes for these two nodes to communicate
+  // back and forth.
+  // Open 'our' pipe for writing
+  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
+
+  if ( role == role_ping_out )
+  {
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);
+  }
+  else
+  {
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1,pipes[0]);
+  }
+
+  radio.startListening(); // start listening
+  
   pinMode(leftLine, INPUT);
   pinMode(rightLine, INPUT);
   pinMode(leftWide, INPUT);
